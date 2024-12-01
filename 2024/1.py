ee0f02b52eb57835f494e11caabd3be1d02e5de3
Reavey <65507003+RaphaelAsla@users.noreply.github.com>
2024-12-02 01:14:25 +0200

    comment submissions

--- 2024/1.py
+++ 2024/1.py
@@ -2,10 +2,11 @@
 
 from aoctools import *
 
+
 def main(aocd: AOCD):
-    ml = aocd.imultilist
-    aocd.p1(sum(abs(x - y) for x, y in zip(sorted(ml[0]), sorted(ml[1]))))
-    aocd.p2(sum(c * ml[1].count(c) for c in ml[0]))
+    l, r = aocd.imultilist
+    #aocd.p1(sum(abs(x - y) for x, y in zip(sorted(l), sorted(r))))
+    #aocd.p2(sum(c * r.count(c) for c in l))
 
 if __name__ == '__main__':
     aocd = AOCD(2024, 1)

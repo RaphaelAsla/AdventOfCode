9c446cd186eb7b68f3a7d3b68fd2cc8282c22145
Reavey <65507003+RaphaelAsla@users.noreply.github.com>
2024-12-02 18:21:33 +0200

    day two

--- 2024/2.py
+++ 2024/2.py
@@ -2,11 +2,28 @@
 
 from aoctools import *
 
+def is_safe(rep):
+    inc = rep[0] < rep[1]
+    for i in range(len(rep) - 1):
+        if not (1 <= abs(rep[i] - rep[i + 1]) <= 3 and (rep[i] < rep[i + 1]) == inc):
+            return False, i
+    return True, -1 
+
+def tolerable(rep, start):
+    return any(is_safe(rep[:i] + rep[i+1:])[0] for i in range(start - 1, len(rep)))
 
 def main(aocd: AOCD):
-    aocd.get_example()
+    a = 0
+    b = 0
 
+    for rep in aocd.irows:
+        safe, failed_at = is_safe(rep)
+        a += safe
+        b += safe or tolerable(rep, failed_at) 
 
-if __name__ == '__main__':
+    aocd.p1(a)
+    aocd.p2(b)
+
+if __name__ == "__main__":
     aocd = AOCD(2024, 2)
     main(aocd)

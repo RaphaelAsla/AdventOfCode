6515f86d9b8661cf95bb57bf888a5ec928d1ce97
Reavey <65507003+RaphaelAsla@users.noreply.github.com>
2024-12-04 00:44:32 +0200

    day three

--- 2024/3.py
+++ 2024/3.py
@@ -1,11 +1,17 @@
 """ Advent Of Code 2024 : 3 """
 
 from aoctools import *
-
+import re
 
 def main(aocd: AOCD):
-    aocd.get_example()
+    a = 0
+    b = 0
 
+    a = sum(int(x) * int(y) for x, y in re.findall(r"mul\((\d+),(\d+)\)", aocd.as_str))
+    b = sum(int(x) * int(y) for x, y in re.findall(r"mul\((\d+),(\d+)\)", re.sub(r"don\'t\(\).*?(?=do\(\))|don\'t\(\).*", "", aocd.as_str.replace('\n', ''))))
+
+    aocd.p1(a)
+    aocd.p2(b)
 
 if __name__ == '__main__':
     aocd = AOCD(2024, 3)

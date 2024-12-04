3cc5e01ca24dd08bd75461f468b0a5acc1b65c5d
Reavey <65507003+RaphaelAsla@users.noreply.github.com>
2024-12-04 18:43:24 +0200

    formatting

--- 2024/4.py
+++ 2024/4.py
@@ -10,16 +10,16 @@ def main(aocd: AOCD):
     b = 0
 
     dirs = {(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)}
-    rot_dirs = [{(-1, 1) : 'M', (1, 1) : 'S', (1, -1) : 'S', (-1, -1):'M'},
-                {(-1, 1) : 'M', (1, 1) : 'M', (1, -1) : 'S', (-1, -1):'S'},
-                {(-1, 1) : 'S', (1, 1) : 'M', (1, -1) : 'M', (-1, -1):'S'},
-                {(-1, 1) : 'S', (1, 1) : 'S', (1, -1) : 'M', (-1, -1):'M'}]
+    rot_dirs = [{(-1, 1) : 'M', (1, 1) : 'S', (1, -1) : 'S', (-1, -1): 'M'},
+                {(-1, 1) : 'M', (1, 1) : 'M', (1, -1) : 'S', (-1, -1): 'S'},
+                {(-1, 1) : 'S', (1, 1) : 'M', (1, -1) : 'M', (-1, -1): 'S'},
+                {(-1, 1) : 'S', (1, 1) : 'S', (1, -1) : 'M', (-1, -1): 'M'}]
 
     grid = aocd.sgrid
 
     for c in grid:
         if grid[c] == 'X':
-            a += sum("MAS" == "".join("".join(grid[c[0] + d[0] * i, c[1] + d[1] * i]) for i in range(1, 4) if in_grid(c[0] + d[0] * i, c[1] + d[1] *i)) for d in dirs)
+            a += sum("MAS" == "".join("".join(grid[c[0] + d[0] * i, c[1] + d[1] * i]) for i in range(1, 4) if in_grid(c[0] + d[0] * i, c[1] + d[1] * i)) for d in dirs)
         elif grid[c] == 'A':
             b += sum(all(in_grid(c[0] + d[0], c[1] + d[1]) and grid[c[0] + d[0], c[1] + d[1]] == dir[d] for d in dir) for dir in rot_dirs)
 

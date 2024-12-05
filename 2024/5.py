967a6f32cac4c6853fe94b918a6f8b6d46b95aed
Reavey <65507003+RaphaelAsla@users.noreply.github.com>
2024-12-05 20:31:34 +0200

    day 5

--- 2024/5.py
+++ 2024/5.py
@@ -1,11 +1,37 @@
 """ Advent Of Code 2024 : 5 """
 
 from aoctools import *
-
+from collections import defaultdict, deque
 
 def main(aocd: AOCD):
-    aocd.get_example()
+    a = 0
+    b = 0
 
+    rules, updates = [x.split('\n') for x in aocd.slist_split_at("\n\n")]
+
+    updates = [s.split(',') for s in updates]
+    max_size = max(len(update) for update in updates)
+
+    before = defaultdict(list)
+    for x, y in [rule.split('|') for rule in rules]:
+        before[y].append(x)
+
+    for update in updates:
+        sorted = True
+        for _ in range(max_size):
+            for i in range(len(update) - 1, 0, -1):
+                for j in range(i):
+                    if update[j] not in before[update[i]]:
+                        update.append(update.pop(j))
+                        sorted = False
+            if sorted:
+                a += int(update[len(update) // 2])
+                break
+        if not sorted:
+            b += int(update[len(update) // 2])
+
+    aocd.p1(a)
+    aocd.p2(b)
 
 if __name__ == '__main__':
     aocd = AOCD(2024, 5)

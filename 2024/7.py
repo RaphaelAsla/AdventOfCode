0553ef5d1b72ca96c22fe93d7979c10759f1e269
Reavey <65507003+RaphaelAsla@users.noreply.github.com>
2024-12-07 18:19:15 +0200

    day 7 (will re-write for a more efficient approach

--- 2024/7.py
+++ 2024/7.py
@@ -1,11 +1,39 @@
 """ Advent Of Code 2024 : 7 """
 
 from aoctools import *
+from itertools import product
 
+def calc(x, eq, p2=False):
+    ans = 0
+    for ops in product(['+', '*', "||"], repeat=(len(eq) - 1)):
+        nxt, *rest = eq
+        for i, op in enumerate(ops):
+            if op == '+':
+                nxt += rest[i]
+            elif op == '*':
+                nxt *= rest[i]
+            elif p2 and op == "||":
+                nxt = int(str(nxt) + str(rest[i]))
+        if p2 and x == nxt:
+            ans += nxt
+            break
+        elif x == nxt:
+            ans += nxt
+            break
+    return ans
 
 def main(aocd: AOCD):
-    aocd.get_example()
+    a = 0
+    b = 0
 
+    for x, eq in list(x.split(": ") for x in aocd.slist):
+        x = int(x)
+        eq = [*map(int, eq.split(' '))]
+        a += calc(x, eq, False)
+        b += calc(x, eq, True)
+
+    aocd.p1(a)
+    aocd.p2(b)
 
 if __name__ == '__main__':
     aocd = AOCD(2024, 7)

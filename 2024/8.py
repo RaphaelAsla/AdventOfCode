185fbad4ceb5063b40c871ff09d43384af9d53e9
Reavey <65507003+RaphaelAsla@users.noreply.github.com>
2024-12-09 02:52:20 +0200

    day 8, decided to make this more intresting and not use iter.combinations, instead I casted rays to find pair antennas

--- 2024/8.py
+++ 2024/8.py
@@ -1,11 +1,58 @@
 """ Advent Of Code 2024 : 8 """
 
 from aoctools import *
+from math import pi, cos, sin
 
+def cast_ray(origin, direction, max_distance, grid):
+    ox, oy = origin
+    antinodes_p1 = set()
+    antinodes_p2 = set()
+
+    for t in range(1, max_distance):
+        pos = Vec(round(ox + t * cos(direction)), round(oy + t * sin(direction)))
+
+        if  pos not in grid or pos == origin:
+            break
+
+        if grid[Vec(ox, oy)] == grid[pos]:
+            diff = origin - pos
+
+            for tt in (1, -2):
+                ant_node = origin + tt * diff
+                if ant_node in grid:
+                    antinodes_p1.add(ant_node)
+
+            for tt in range(-48, 49):
+                ant_node = origin + tt * diff
+                if ant_node in grid:
+                    antinodes_p2.add(ant_node)
+                   
+    return antinodes_p1, antinodes_p2
+
+def drange(start, stop, step):
+    r = start
+    while r < stop:
+        yield r
+        r += step
 
 def main(aocd: AOCD):
-    aocd.get_example()
+    grid = aocd.vsgrid
 
+    a = set()
+    b = set()
+
+    for pos in grid:
+        if grid[pos] == '.':
+            continue
+
+        for dir in drange(0.0, 2 * pi, pi / (aocd.grid_width * 2 - 2)):
+            max_distance = aocd.grid_width
+            hit_antinodes = cast_ray(pos, dir, max_distance, grid)
+            a.update(hit_antinodes[0])
+            b.update(hit_antinodes[1])
+
+    aocd.p1(len(a))
+    aocd.p2(len(b))
 
 if __name__ == '__main__':
     aocd = AOCD(2024, 8)

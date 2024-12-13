a04d781981c3bbc36911bccb65b221c2e349a632
Reavey <65507003+RaphaelAsla@users.noreply.github.com>
2024-12-13 18:46:19 +0200

    day 13

--- 2024/13.py
+++ 2024/13.py
@@ -10,25 +10,23 @@ def main(aocd: AOCD):
     a = 0
     b = 0
 
-    buttons_prizes = []
+    buttons_nprize = []
     for entry in data:
-        buttons = []
         x1_inc = int(entry[0].split("X+")[1].split(",")[0])
         y1_inc = int(entry[0].split("Y+")[1])
         x2_inc = int(entry[1].split("X+")[1].split(",")[0])
         y2_inc = int(entry[1].split("Y+")[1])
-        buttons.append(np.array([x1_inc, x2_inc]))
-        buttons.append(np.array([y1_inc, y2_inc]))
+        buttons = [[x1_inc, x2_inc], [y1_inc, y2_inc]]
         prize = entry[2]
         x = int(prize.split("X=")[1].split(",")[0])
         y = int(prize.split("Y=")[1])
-        buttons_prizes.append([buttons, np.array([x, y])])
+        buttons_nprize.append([np.linalg.inv(buttons), np.array([x, y])])
 
     error = 0.01
     c = 10000000000000
-    for res in buttons_prizes:
-        p1b1, p1b2 = np.linalg.inv(res[0]).dot(res[1])
-        p2b1, p2b2 = np.linalg.inv(res[0]).dot(res[1] + c)
+    for res in buttons_nprize:
+        p1b1, p1b2 = np.dot(res[0], res[1])
+        p2b1, p2b2 = np.dot(res[0], res[1] + c)
         if abs(p1b1 - round(p1b1)) < error and abs(p1b2 - round(p1b2)) < error:
             p1b1, p1b2 = round(p1b1), round(p1b2)
             a += p1b1 * 3 + p1b2

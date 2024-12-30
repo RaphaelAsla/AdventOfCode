c341c7c7d3d35a21f5edf55b69268672e07d22ab
Reavey <65507003+RaphaelAsla@users.noreply.github.com>
2024-12-30 17:47:21 +0200

    day 25 part 1

--- 2024/25.py
+++ 2024/25.py
@@ -2,10 +2,23 @@
 
 from aoctools import *
 
-
 def main(aocd: AOCD):
-    aocd.get_example()
+    a = 0
+    b = 0
 
+    entries = aocd.slist_split_at("\n\n")
+
+    locks = []
+    keys = []
+
+    for entry in entries:
+        m = entry.split('\n')
+        temp = [sum(1 for y in range(len(m)) if m[y][x] == '#') - 1 for x in range(len(m[0]))]
+        (locks if m[0][0] == '#' else keys).append(temp)
+
+    a = sum(1 for lock in locks for key in keys if all((a + b) <= 5 for a, b in zip(lock, key)))
+
+    aocd.p1(a)
 
 if __name__ == '__main__':
     aocd = AOCD(2024, 25)

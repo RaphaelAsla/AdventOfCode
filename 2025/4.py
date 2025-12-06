e79448c2e766f4435b0215baefb4d24983b8a869
Reavey <65507003+RaphaelAsla@users.noreply.github.com>
2025-12-07 01:20:47 +0200

    2025 day 4

--- 2025/4.py
+++ 2025/4.py
@@ -1,15 +1,49 @@
-""" Advent Of Code 2025 : 4 """
+"""Advent Of Code 2025 : 4"""
 
 from aoctools import *
 
 
 def main(aocd: AOCD):
-    aocd.get_example()
+    inp = aocd.sgrid
+
+    p1 = 0
+    p2 = 0
+
+    DIRS_8 = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+
+    first_iter = True
+
+    while True:
+        to_remove = []
+
+        for pos, v in inp.items():
+            if v != "@":
+                continue
+
+            adj = sum(inp.get((pos[0] + dx, pos[1] + dy), ".") == "@" for dx, dy in DIRS_8)
+
+            if adj < 4:
+                if first_iter:
+                    p1 += 1
+                to_remove.append(pos)
+
+        if not to_remove:
+            break
+
+        for pos in to_remove:
+            inp[pos] = "."
+            p2 += 1
+
+        first_iter = False
+
+    aocd.p1(p1)
+    aocd.p2(p2)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import time
+
     start = time.time()
     aocd = AOCD(2025, 4)
     main(aocd)
-    print(f'Time Taken: {time.time() - start} seconds')
+    print(f"Time Taken: {time.time() - start} seconds")

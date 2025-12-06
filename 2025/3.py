c7c1bb219f5d82694bff78b29bfc7f475673cb52
Reavey <65507003+RaphaelAsla@users.noreply.github.com>
2025-12-06 13:45:22 +0200

    rest (template) days)

---
+++ 2025/3.py
@@ -0,0 +1,15 @@
+""" Advent Of Code 2025 : 3 """
+
+from aoctools import *
+
+
+def main(aocd: AOCD):
+    aocd.get_example()
+
+
+if __name__ == '__main__':
+    import time
+    start = time.time()
+    aocd = AOCD(2025, 3)
+    main(aocd)
+    print(f'Time Taken: {time.time() - start} seconds')

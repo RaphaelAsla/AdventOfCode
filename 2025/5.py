fc0f5bf30b65ae5353eddc713cedce1748555f96
Reavey <65507003+RaphaelAsla@users.noreply.github.com>
2025-12-07 17:53:22 +0200

    2025 day 5

--- 2025/5.py
+++ 2025/5.py
@@ -4,8 +4,29 @@ from aoctools import *
 
 
 def main(aocd: AOCD):
-    aocd.get_example()
+    ranges, ids = [[list(map(int, r.split('-'))) if '-' in r else int(r) for r in x.split('\n')] for x in aocd.slist_split_at('\n\n')]
+    ranges = sorted(ranges, key=lambda x: x[0])
 
+    a = 0
+    b = 0
+
+    merged = []
+    for start, end in ranges:
+        if merged and start <= merged[-1][1]:
+            merged[-1] = (merged[-1][0], max(merged[-1][1], end))
+        else:
+            merged.append((start, end))
+
+    for id in ids:
+        for r in merged:
+            if id >= r[0] and id <= r[1]:
+                a += 1
+
+    for r in merged:
+        b += r[1] - r[0] + 1
+
+    aocd.p1(a)
+    aocd.p2(b)
 
 if __name__ == '__main__':
     import time

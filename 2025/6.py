7f9db8a5b725fced03ca292e7d8857b9ecf59804
Reavey <65507003+RaphaelAsla@users.noreply.github.com>
2025-12-08 03:01:45 +0200

    2025 day 6

--- 2025/6.py
+++ 2025/6.py
@@ -1,15 +1,37 @@
-""" Advent Of Code 2025 : 6 """
+"""Advent Of Code 2025 : 6"""
 
 from aoctools import *
+from itertools import groupby
+from math import prod
 
 
 def main(aocd: AOCD):
-    aocd.get_example()
+    inp = aocd.slist_split_at("\n")
+    nums = [list(map(int, line.split())) for line in inp[:-1]]
+    syms = inp[-1].split()
+
+    a = 0
+    b = 0
+
+    for i in range(len(nums[0])):
+        values = [int(row[i]) for row in nums if row[i]]
+        a += prod(values) if syms[i] == "*" else sum(values)
+
+    nums = [list(line) for line in inp[:-1]]
+
+    it = ["".join(row[x] for row in nums if row[x].isdigit()) for x in range(len(nums[0]))]
+    prob = [list(map(int, group)) for k, group in groupby(it, key=lambda x: x != "") if k]
+
+    b += sum(prod(v) if syms[i] == "*" else sum(v) for i, v in enumerate(prob))
+
+    aocd.p1(a)
+    aocd.p2(b)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import time
+
     start = time.time()
     aocd = AOCD(2025, 6)
     main(aocd)
-    print(f'Time Taken: {time.time() - start} seconds')
+    print(f"Time Taken: {time.time() - start} seconds")

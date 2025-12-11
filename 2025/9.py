7d5e18780fd15208a3cfd8c2baf363820f8ba0e0
Reavey <65507003+RaphaelAsla@users.noreply.github.com>
2025-12-12 00:59:03 +0200

    2025 day 9 -- part 1 only

--- 2025/9.py
+++ 2025/9.py
@@ -4,7 +4,24 @@ from aoctools import *
 
 
 def main(aocd: AOCD):
-    aocd.get_example()
+    #aocd.get_example()
+
+    inp = aocd.irows_split_at(',')
+
+    a = 0
+    #b = 0
+
+    for i in inp:
+        for j in inp:
+            if i != j:
+                x_diff = abs(i[0] - j[0]) + 1
+                y_diff = abs(i[1] - j[1]) + 1
+                t = x_diff if x_diff > 0 else 1
+                t *= y_diff if y_diff > 0 else 1
+                a = max(a, t)
+
+    aocd.p1(a)
+    #aocd.p2(b)
 
 
 if __name__ == '__main__':

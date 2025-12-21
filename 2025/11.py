759fad1d7c4ab3ea860a72395138ca186c477a8b
Reavey <65507003+RaphaelAsla@users.noreply.github.com>
2025-12-21 03:51:04 +0200

    2025 day 11

--- 2025/11.py
+++ 2025/11.py
@@ -1,11 +1,32 @@
 """ Advent Of Code 2025 : 11 """
 
 from aoctools import *
+from functools import lru_cache
 
 
 def main(aocd: AOCD):
-    aocd.get_example()
+    inp = aocd.dict_split_at(': ')
 
+    a = 0
+    b = 0 
+
+    tree = {k: v.split() for k, v in inp.items()}
+
+    @lru_cache(None)
+    def dfs(node, seen_dac, seen_fft):
+        if node == "out":
+            return int(seen_dac and seen_fft)
+
+        total = 0
+        for nxt in tree.get(node, []):
+            total += dfs(nxt, seen_dac or node == "dac", seen_fft or node == "fft")
+        return total
+
+    a = dfs("you", True, True)
+    b = dfs("svr", False, False)
+
+    aocd.p1(a)
+    aocd.p2(b)
 
 if __name__ == '__main__':
     import time

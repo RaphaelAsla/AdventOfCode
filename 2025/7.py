688e5f820ca59ef249b4bbffa243c3caa293ebb3
Reavey <65507003+RaphaelAsla@users.noreply.github.com>
2025-12-21 03:51:19 +0200

    use lru_cache

--- 2025/7.py
+++ 2025/7.py
@@ -1,25 +1,8 @@
 """ Advent Of Code 2025 : 7 """
 
 from aoctools import *
+from functools import lru_cache
 
-
-def dfs(y, x, grid, memo):
-    if not (0 <= y < len(grid) and 0 <= x < len(grid[0])):
-        return 1
-
-    if (y, x) in memo:
-        return memo[(y, x)]
-
-    cell = grid[y][x]
-    if cell == '.':
-        res = dfs(y + 1, x, grid, memo)
-    elif cell == '^':
-        res = dfs(y + 1, x - 1, grid, memo) + dfs(y + 1, x + 1, grid, memo)
-    else:
-        res = 0
-
-    memo[(y, x)] = res
-    return res
 
 def main(aocd: AOCD):
     inp = [list(r.replace('S', '|')) for r in aocd.slist_split_at('\n')]
@@ -40,8 +23,20 @@ def main(aocd: AOCD):
 
     inp = [inp[0]] + [['.' if c == '|' else c for c in row] for row in inp[1:]]
 
-    memo = {}
-    b = dfs(1, inp[0].index('|'), inp, memo)
+    @lru_cache(None)
+    def dfs(y, x):
+        if not (0 <= y < len(inp) and 0 <= x < len(inp[0])):
+            return 1
+
+        cell = inp[y][x]
+        if cell == '.':
+            return dfs(y + 1, x)
+        elif cell == '^':
+            return dfs(y + 1, x - 1) + dfs(y + 1, x + 1)
+        else:
+            return 0
+
+    b = dfs(1, inp[0].index('|'))
 
     aocd.p1(a)
     aocd.p2(b)
